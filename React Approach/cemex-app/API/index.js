fc8aef8b61ddf6a172eec8c3f2b10381c03dba5a
abiel-lozano <73564559+abiel-lozano@users.noreply.github.com>
2022-06-03 18:19:52 -0500

    Incomplete Functions

--- React Approach/cemex-app/API/index.js
+++ React Approach/cemex-app/API/index.js
@@ -78,3 +78,41 @@ async function connectToDb(){
 	}
 }
 
+export async function getUsers(req, res) {
+	const users = await User.find();
+	res.json(users);
+}
+
+export async function getUser(req, res) {
+	const users = await User.findById(req.params.id);
+	res.json(users);
+}
+
+export async function postUsers(req, res) {
+	const { name, gamertag, points } = req.body;
+	const user = new User({name, email, dob, passwordHash, score, helmetNum, ordinaryNum, generalNum, totalNum, coins, admin, numAchUnlocked, weapon1, weapon2, weapon3, weapon4});
+	// Uno de estos dos:
+	var email = req.query.email;
+	var email = req.params.email;
+	await user.save();
+	res.json(user);
+}
+
+export async function putUsers(req, res) {
+	const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+	});
+	res.json(user);
+}
+// no funciona
+export async function deleteUsers(req, res) {
+	const user = await User.findByIdAndDelete(req.params.id);
+	// res.json(user);
+}
+
+router.get("/users", getUsers);
+router.get("/users/:id", getUser);
+router.post("/users", postUsers);
+router.put("/users/:id", putUsers);
+router.delete("/users/:id", deleteUsers);
+
