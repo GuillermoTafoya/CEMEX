7165594fc03866f9dce3b2f469283ddf9b1e5502
Guillermo Tafoya <A01633790@tec.mx>
2022-10-10 16:18:46 -0500

    Update api.db.js

--- React Approach/cemex-app/src/API/api.db.js
+++ React Approach/cemex-app/src/API/api.db.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
-// const URI = "mongodb+srv://A01284213:cyberpunksAdmin@web-be-cluster.ti48frj.mongodb.net/?retryWrites=true&w=majority";
-const URI = "mongodb+srv://mauricio:cuadros@cluster0.8jlk6.mongodb.net/Cemex-App-Cyberpunks?retryWrites=true&w=majority";
+const URI = "ATLASMONGODB";
 
 export async function connectToDb(){
 	try {
